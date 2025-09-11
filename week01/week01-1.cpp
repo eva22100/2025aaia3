2476b5d331b1304a8cfb65fa500f12d648263192
青空 <128980708+eva22100@users.noreply.github.com>
2025-09-11 17:39:21 +0800

    week01

---
+++ week01/week01-1.cpp
@@ -0,0 +1,11 @@
+//week01-1
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        int H=haystack.length(),N=needle.length();
+        for(int i=0; i<=H-N;i++){
+            if(haystack.substr(i,N)==needle) return i;
+        }
+        return -1 ;
+    }
+};
---
+++ week01/week01-3c.cpp
@@ -0,0 +1,12 @@
+//week01-3c.cpp
+# include <stdio.h>//使用c++的外掛,負責輸入input 輸出output
+int main()
+{
+    int n;
+    scanf("%d",&n);//使用c語言的scan with format讀入
+    int ans=0;
+    for(int i=1;i<=n;i++){
+        ans+=i*i;
+    }
+    printf("%d",ans);//使用c語言的scan with format印出來
+}
