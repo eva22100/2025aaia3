64f52a374be89aebd86b9463c57ba335b760c1e6
青空 <128980708+eva22100@users.noreply.github.com>
2025-10-02 17:18:25 +0800

    Add files via upload

---
+++ week04/week04-4b.cpp
@@ -0,0 +1,19 @@
+//week04-4b.cpp
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	while (cin>>a>>b){
+		int ans=0;
+		int now=1;
+		while(n!=1){
+			if(n%2==1)n=3*n+1;
+			else n=n/2;
+			now++;
+		}
+		if(now>ans)ans=now;
+	}
+
+		printf("%d %d %d\n",a,b,ans);
+}
