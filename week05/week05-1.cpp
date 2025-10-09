557daaa478e737fe8bc5b5ae283766fa0e043354
青空 <128980708+eva22100@users.noreply.github.com>
2025-10-09 17:08:36 +0800

    week05

---
+++ week05/week05-1.cpp
@@ -0,0 +1,14 @@
+//week05-1.cpp
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        stringstream ss(s);
+        string word;
+        //ss>>word;
+        //cout<<"Åª¨ì¤F"<<word<<"\n";
+        while(ss>>word){
+
+        }
+        return word.length();
+    }
+};
---
+++ week05/week05-3a.cpp
@@ -0,0 +1,11 @@
+///week05-3a.cpp
+#include <iostream>
+using namespace std;
+int main()
+{
+    string line;
+    while(getline(cin,line)){
+
+        cout<<line<<endl;
+    }
+}
